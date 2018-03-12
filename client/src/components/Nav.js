c0c1bacbba9867841c352f49cbd7e7a01a36e171
Nicholas Sladden <nickoliasxii@gmail.com>
2018-03-12 17:54:47 -0500

    stuck on getting nav to work

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,17 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom"
 
 const Nav = () =>
-  <nav className="navbar">
-    <div className="container">
-      <div className="navbar-header">
-        <Link to="/" className="navbar-brand">
+  <div className="container-fluid bg-danger">
+    <div className="navbar-header">
+      <a className="navbar-brand" id="navbarlogo" href="/">
+        <img
+          src={require("../assets/images/The_New_York_Times_logo.png")}
+          alt="NewYorkTimes"
+          height="55"
+        />
+      </a>
+      Article Saver
+    </div>
+    <div
+      className="collapse navbar-collapse justify-content-end"
+      id="navbarNavAltMarkup"
+    >
+      <div className="nav navbar-nav navbar-right">
+        <Link to="/" className="nav-item nav-link">
           Home
         </Link>
-        <Link to="/saved" className="navbar-brand">
+        <Link to="/saved" className="nav-item nav-link">
           Saved
         </Link>
       </div>
     </div>
-  </nav>;
+  </div>
+  ;
 
 export default Nav;
